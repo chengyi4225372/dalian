15fe0f6e1cab5186d302e69f9608d5bc381ace06
chengyi <732345907@qq.com>
2020-05-13 17:29:18 +0800

    前台基本完成

--- application/config.php
+++ application/config.php
@@ -132,9 +132,6 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__CSS__'=>'static/home/css',
-        '__JS__'=>'static/home/js',
-        '__IMAGES__'=>'static/home/images',
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
