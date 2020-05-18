3683724453c61a3feff11eab4b23b27a5546a094
wangyi <460682428@qq.com>
2020-05-18 10:16:17 +0800

    add

--- application/v1/controller/Banner.php
+++ application/v1/controller/Banner.php
@@ -16,28 +16,73 @@ use app\v1\controller\Base;
 class Banner extends Base {
 
     protected $table= 'banner';
-
-
+    
     public function index(){
-
+        $list = Db::name($this->table)->where(['status'=>1])->order('id desc')->paginate(15);
+        $this->assign('list',$list);
         return $this->fetch();
     }
-
-
+    
     public function add(){
+        if($this->request->isPost()){
+            $imgs = input('post.img','','trim');
+            if(empty($imgs)){
+                return false;
+            }
+            $ret = Db::name($this->table)->insertGetId(['imgs'=>$imgs,'create_time'=>time()]);
 
+            if($ret !== false){
+                return json(['code'=>200,'msg'=>'添加成功']);
+            }else {
+                return json(['code'=>400,'msg'=>'添加失败']);
+            }
+        }
         return $this->fetch();
     }
-
-
+    
     public function edit(){
+        if($this->request->isPost()){
+            $mid = input('post.mid','','int');
+            $imgs= input('post.imgs','','trim');
 
+            if(empty($mid) || !isset($mid)){
+                return false;
+            }
+
+            $res= Db::name($this->table)->where(['id'=>$mid])->update(['imgs'=>$imgs]);
+
+            if($res !== false){
+                return json(['code'=>200,'msg'=>'编辑成功']);
+            }else {
+                return json(['code'=>400,'msg'=>'编辑失败']);
+            }
+
+        }
+        $mid = input('get.mid','','int');
+        if(empty($mid) || !isset($mid)){
+            return false;
+        }
+        $info = Db::name($this->table)->where(['id'=>$mid])->find();
+        $this->assign('info',$info);
         return $this->fetch();
     }
-
+    
     public function del(){
+        if($this->request->isGet()){
+            $mid = input('get.mid','','int');
+            if(empty($mid) || !isset($mid)){
+                return false;
+            }
 
-        return $this->fetch();
+            $ret = Db::name($this->table)->where(['id'=>$mid])->update(['status'=>0]);
+
+            if($ret !== false){
+                return json(['code'=>200,'msg'=>'删除成功']);
+            }else{
+                return json(['code'=>400,'msg'=>'删除失败']);
+            }
+        }
+        return false;
     }
 
     /**
